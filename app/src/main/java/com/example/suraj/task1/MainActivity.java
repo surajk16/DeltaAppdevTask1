a8155b5e40b0a904932dba05ad470de3b6fc8857
surajk16 <suraj.anitha@gmail.com>
2017-05-30 19:30:59 +0530

    Added save data for orientatin change and app kill

--- app/src/main/java/com/example/suraj/task1/MainActivity.java
+++ app/src/main/java/com/example/suraj/task1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.suraj.task1;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int r=0,g=0,b=0;
+    int r = 0, g = 0, b = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,41 +18,121 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void change (View v)
-    {
-        if (v.getId()==R.id.button)
-        {
-            r+=10;
-            if (r>255) r=0;
+    public void change(View v) {
+        if (v.getId() == R.id.button) {
+            r += 10;
+            if (r > 255) r = 0;
 
             TextView t = (TextView) findViewById(R.id.textView);
-            t.setText(""+r);
+            t.setText("" + r);
 
             RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
             rl.setBackgroundColor(Color.rgb(r, g, b));
-        }
-
-        else if (v.getId()==R.id.button2)
-        {
-            g+=10;
-            if (g>255) g=0;
+        } else if (v.getId() == R.id.button2) {
+            g += 10;
+            if (g > 255) g = 0;
 
             TextView t = (TextView) findViewById(R.id.textView2);
-            t.setText(""+g);
+            t.setText("" + g);
 
             RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
             rl.setBackgroundColor(Color.rgb(r, g, b));
-        }
-
-        else if (v.getId()==R.id.button3)
-        {
-            b+=10;
-            if (b>255) b=0;
+        } else if (v.getId() == R.id.button3) {
+            b += 10;
+            if (b > 255) b = 0;
 
             TextView t = (TextView) findViewById(R.id.textView3);
-            t.setText(""+b);
+            t.setText("" + b);
 
             RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
             rl.setBackgroundColor(Color.rgb(r, g, b));
         }
+
     }
+
+        public void reset (View v)
+    {
+        r=0; g=0; b=0;
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("" + r);
+        t = (TextView) findViewById(R.id.textView2);
+        t.setText("" + g);
+        t = (TextView) findViewById(R.id.textView3);
+        t.setText("" + b);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        rl.setBackgroundColor(Color.rgb(r, g, b));
+
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putInt("r", r);
+        savedInstanceState.putInt("g", g);
+        savedInstanceState.putInt("b", b);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        r = savedInstanceState.getInt("r");
+        g = savedInstanceState.getInt("g");
+        b = savedInstanceState.getInt("b");
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("" + r);
+        t = (TextView) findViewById(R.id.textView2);
+        t.setText("" + g);
+        t = (TextView) findViewById(R.id.textView3);
+        t.setText("" + b);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        rl.setBackgroundColor(Color.rgb(r, g, b));
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences pref;
+        pref = getSharedPreferences("info", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("r",r);
+        editor.putInt("g",g);
+        editor.putInt("b",b);
+
+        editor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences shared = getSharedPreferences("info",MODE_PRIVATE);
+
+        r = shared.getInt("r",0);
+        g = shared.getInt("g",0);
+        b = shared.getInt("b",0);
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText("" + r);
+        t = (TextView) findViewById(R.id.textView2);
+        t.setText("" + g);
+        t = (TextView) findViewById(R.id.textView3);
+        t.setText("" + b);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        rl.setBackgroundColor(Color.rgb(r, g, b));
+
+        
+    }
+
+}
